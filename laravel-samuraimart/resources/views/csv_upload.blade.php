db38c0ba02423ef30e68669e7042e01d8a4cf0c4
yo447 <kakuto1019@gmail.com>
2025-10-08 04:25:31 +0900

    Remove all files from repository

--- laravel-samuraimart/resources/views/csv_upload.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="btn-group pull-right" style="margin-right: 10px">
-   <a href="#" class="btn btn-sm btn-primary csv-import"><i class="fa fa-upload"></i><span class="hidden-xs"> CSVインポート</span></a>
-   <input type="file" id="files" name="product" style="display: none">
-</div>
